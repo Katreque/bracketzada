81a6e4e725259a43f9eee1e6d2f75598a1cc5233
Renan Verissimo de vasconcelos <renanverissimo@uol.com.br>
2018-11-13 11:29:57 -0200

    Estruturação do projeto pronta.

---
+++ gulpfile.babel.js
@@ -0,0 +1,40 @@
+import gulp from 'gulp';
+import mocha from 'gulp-mocha';
+import babel from 'gulp-babel';
+import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+import del from 'del';
+
+gulp.task('build_commonjs', function (done) {
+  return gulp.src('./build/bracketzada.js')
+        .pipe(babel({
+          presets: [
+            '@babel/preset-env'
+          ]
+        }))
+        .pipe(uglify())
+        .pipe(rename({
+          suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist/commonjs'))
+        .on('end', () => done())
+});
+
+gulp.task('unit_test', function () {
+  return gulp.src('./build/*.test.js')
+      .pipe(mocha({
+        reporter: 'spec'
+      }))
+});
+
+gulp.task('del', function () {
+  return del([
+    './dist'
+  ])
+});
+
+gulp.task('build', gulp.series(
+  'del',
+  'unit_test',
+  'build_commonjs'
+));
